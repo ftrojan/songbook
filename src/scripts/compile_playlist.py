a4f0eab03f7ddb966f8830eb2cf38df374038d24
filip <f.trojan@centrum.cz>
2024-09-28 14:40:59 +0200

    playlist

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("orion_2024-09-07")
+p = playlist.get_playlist("orion_2024-11-16")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
