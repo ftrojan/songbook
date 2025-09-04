c4b47f5181b4df9f1dc328cb0d1f7063f3885109
filiptrojandatasentics <filip.trojan@datasentics.cz>
2025-09-04 17:28:05 +0200

    potlach

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("orion_2025-06-26")
+p = playlist.get_playlist("orion_2025-09-06_potlach")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
