62b613c930ba0bc04fabd7ad14ea6cec6b89f413
filiptrojandatasentics <filip.trojan@datasentics.cz>
2025-10-23 16:12:09 +0200

    2025-11-07

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("orion_2025-10-09")
+p = playlist.get_playlist("orion_2025-11-07")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
