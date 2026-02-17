dc93a37f0d29b91facb73f957f3c8e31b9265502
filiptrojandatasentics <filip.trojan@datasentics.cz>
2026-02-17 17:06:18 +0100

    zloukovice playlist update

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("svatba")
+p = playlist.get_playlist("orion_2026-03-13")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
