321bdd6609e50ce89cfb0b00f80748fca3983dbd
filiptrojandatasentics <filip.trojan@datasentics.cz>
2026-05-08 11:20:28 +0200

    busking update

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("svatba_2026-04-25")
+p = playlist.get_playlist("busking")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
