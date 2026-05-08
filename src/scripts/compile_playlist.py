e82fe2eafc035dc19e4147737a2fa1796655ca75
filiptrojandatasentics <filip.trojan@datasentics.cz>
2026-05-08 07:26:50 +0200

    black orpheus update

--- src/scripts/compile_playlist.py
+++ src/scripts/compile_playlist.py
@@ -3,6 +3,6 @@ from typeset import playlist
 
 
 logging.basicConfig(level="INFO")
-p = playlist.get_playlist("svatba")
+p = playlist.get_playlist("svatba_2026-04-25")
 playlist.typeset_playlist(p)
 logging.info(f"output saved to {p.output_path}")
